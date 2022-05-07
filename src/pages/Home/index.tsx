d2e4a604fb755e959f21a39e946a2697d0afe7f8
Elyas <touahria.elyas@outlook.com>
2022-05-07 23:00:08 +0200

    init project

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+  return <div>On revient demain</div>;
+};
+
+export default Home;
---
+++ src/pages/Survey.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Survey: React.FC = () => {
+  return <div>Teste</div>;
+};
+
+export default Survey;
